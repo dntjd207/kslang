beccbd67057a08171432e1a7548acd57bd9aeb31
parkdongju <dntjd207@naver.com>
2026-03-01 12:12:50 +0900

    Add favicon support across all layouts by linking favicon.ico in admin, auth, public, and webview templates.

--- resources/views/layouts/webview.blade.php
+++ resources/views/layouts/webview.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'kslang')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css'])
 </head>
 <body class="bg-white text-gray-900 p-4">
