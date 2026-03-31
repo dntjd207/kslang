ec30d9b29b4c76a86d810061f0897b178e8409c9
박동주 <dntjd207@naver.com>
2026-03-31 09:31:22 +0900

    Add search, random, and daily routes for slangs in API
    - Introduced new API routes for slangs: `slangs/search`, `slangs/random`, and `slangs/daily`.
    - Enhanced the `SlangController` to handle requests for these new endpoints, improving the functionality of the slang management system.
    - Updated documentation to include the new routes and their usage for developers.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\PageController;
 use App\Http\Controllers\Api\V1\SlangController;
 Route::prefix('v1')->group(function () {
+    
     Route::get('slangs/search', [SlangController::class, 'search']);
     Route::get('slangs/random', [SlangController::class, 'random']);
     Route::get('slangs/daily', [SlangController::class, 'daily']);
